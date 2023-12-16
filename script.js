d52e83e81ab8e944aab4d428d0ad9ce68ab63c13
vikash-pandey1 <vikashpandey7082@gmail.com>
2023-12-16 22:04:17 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let add = document.querySelector('.add');
+let double = document.querySelector('.double');
+let show = document.querySelector('show');
+let sort = document.querySelector('sort');
+let total = document.querySelector('.total');
+
+let mid = document.querySelector('.mid');
+
+
+let main = document.querySelector(".mid");
+
+function addTask() {
+    let h2 = document.createElement("h2");
+    h2.innerText = "william son";
+    mid.appendChild(h2);
+}
+
